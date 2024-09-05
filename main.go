cc1295859021985949584b30ac4728e7c017f981
andmoreduro <safic913@gmail.com>
2024-09-04 23:50:50 -0500

    Se cambió la forma de crear los objetos de la interfaz porque el método anterior no consideraba el hecho de que usar la misma variable en varios lugares no la copia, sino que pasan como referencia.

--- main.go
+++ main.go
@@ -8,106 +8,142 @@ import (
 	"image/color"
 )
 
+const windowWidth = 800
+const windowHeight = 600
+
+//type Grass struct{}
+
+func createGrassObject() fyne.CanvasObject {
+	background := canvas.NewRectangle(color.RGBA{R: 124, G: 200, B: 0, A: 255})
+	background.SetMinSize(fyne.NewSize(windowWidth/2-windowHeight/10, windowHeight/3))
+	return background
+}
+
+//type Road struct{}
+
+func createRoadObject(main bool) fyne.CanvasObject {
+	background := canvas.NewRectangle(color.RGBA{R: 18, G: 10, B: 5, A: 255})
+	line := canvas.NewRectangle(color.RGBA{R: 255, G: 255, B: 255, A: 255})
+	if main {
+		background.SetMinSize(fyne.NewSize(windowWidth+10, windowHeight/5))
+		line.SetMinSize(fyne.NewSize(windowWidth+10, windowHeight/100))
+	} else {
+		background.SetMinSize(fyne.NewSize(windowHeight/5, windowHeight/3))
+		line.SetMinSize(fyne.NewSize(windowHeight/100, windowHeight/3))
+	}
+	return container.NewCenter(background, line)
+}
+
 type Semaphore struct {
-	color         color.RGBA
-	previousColor color.RGBA
+	redLight      *canvas.Rectangle
+	yellowLight   *canvas.Rectangle
+	greenLight    *canvas.Rectangle
+	activeLight   string
+	previousLight string
 }
 
 func (semaphore *Semaphore) init() {
-	semaphore.color = color.RGBA{R: 255}
-	semaphore.previousColor = color.RGBA{R: 255, G: 255}
+	semaphore.redLight = canvas.NewRectangle(color.RGBA{R: 255, A: 255})
+	semaphore.redLight.SetMinSize(fyne.NewSize(20, 20))
+	semaphore.yellowLight = canvas.NewRectangle(color.RGBA{R: 255, G: 255, A: 60})
+	semaphore.yellowLight.SetMinSize(fyne.NewSize(20, 20))
+	semaphore.greenLight = canvas.NewRectangle(color.RGBA{G: 255, A: 60})
+	semaphore.greenLight.SetMinSize(fyne.NewSize(20, 20))
+	semaphore.activeLight = "red"
+	semaphore.previousLight = "yellow"
 }
 
-func (semaphore *Semaphore) switchLight() {
-	redLightColor := color.RGBA{R: 255, A: 255}
-	yellowLightColor := color.RGBA{R: 255, G: 255, A: 255}
-	greenLightColor := color.RGBA{G: 255, A: 255}
-	if semaphore.color == redLightColor {
-		semaphore.color = yellowLightColor
-	} else if semaphore.color == greenLightColor {
-		semaphore.color = yellowLightColor
-	} else if semaphore.previousColor == greenLightColor {
-		semaphore.color = redLightColor
+// Inicializa el semáforo y retorna el objeto que lo representa gráficamente
+func createSemaphoreObject(semaphore *Semaphore, vertical bool, inverted bool) fyne.CanvasObject {
+	background := canvas.NewRectangle(color.RGBA{R: 100, G: 100, B: 100, A: 255})
+	if vertical {
+		background.SetMinSize(fyne.NewSize(20, 70))
 	} else {
-		semaphore.color = greenLightColor
+		background.SetMinSize(fyne.NewSize(70, 20))
 	}
-	semaphore.previousColor = semaphore.color
+	var lightContainer *fyne.Container
+	if vertical {
+		lightContainer = container.NewVBox()
+	} else {
+		lightContainer = container.NewHBox()
+	}
+	if inverted {
+		lightContainer.Add(semaphore.greenLight)
+		lightContainer.Add(semaphore.yellowLight)
+		lightContainer.Add(semaphore.redLight)
+	} else {
+		lightContainer.Add(semaphore.redLight)
+		lightContainer.Add(semaphore.yellowLight)
+		lightContainer.Add(semaphore.greenLight)
+	}
+	return container.NewCenter(
+		background,
+		lightContainer,
+	)
+}
+
+// Cambia la luz del semáforo
+func (semaphore *Semaphore) switchLight() {
+	if semaphore.activeLight == "red" {
+		semaphore.redLight = canvas.NewRectangle(color.RGBA{R: 255, A: 60})
+		semaphore.yellowLight = canvas.NewRectangle(color.RGBA{R: 255, G: 255, A: 255})
+		semaphore.activeLight = "yellow"
+		semaphore.previousLight = "red"
+	} else if semaphore.activeLight == "green" {
+		semaphore.greenLight = canvas.NewRectangle(color.RGBA{G: 255, A: 60})
+		semaphore.yellowLight = canvas.NewRectangle(color.RGBA{R: 255, G: 255, A: 255})
+		semaphore.activeLight = "yellow"
+		semaphore.previousLight = "green"
+	} else if semaphore.previousLight == "red" {
+		semaphore.yellowLight = canvas.NewRectangle(color.RGBA{R: 255, G: 255, A: 60})
+		semaphore.greenLight = canvas.NewRectangle(color.RGBA{G: 255, A: 255})
+		semaphore.activeLight = "green"
+		semaphore.previousLight = "yellow"
+	} else {
+		semaphore.yellowLight = canvas.NewRectangle(color.RGBA{R: 255, G: 255, A: 60})
+		semaphore.redLight = canvas.NewRectangle(color.RGBA{R: 255, A: 255})
+		semaphore.activeLight = "red"
+		semaphore.previousLight = "yellow"
+	}
 }
 
 func main() {
-	const width = 800
-	const height = 600
-
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Simulación de Cruce con Semáforo")
-	myWindow.Resize(fyne.NewSize(width, height))
+	myWindow.Resize(fyne.NewSize(windowWidth, windowHeight))
 
-	leftGrass := canvas.NewRectangle(color.RGBA{R: 124, G: 200, B: 0, A: 255})
-	leftGrass.SetMinSize(fyne.NewSize(width/2-height/10, height/3))
-
-	rightGrass := canvas.NewRectangle(color.RGBA{R: 124, G: 200, B: 0, A: 255})
-	rightGrass.SetMinSize(fyne.NewSize(width/2-height/10, height/3))
-
-	secondaryRoad := canvas.NewRectangle(color.RGBA{R: 18, G: 10, B: 5, A: 255})
-	secondaryRoad.SetMinSize(fyne.NewSize(height/5, height/3))
-
-	secondaryRoadLine := canvas.NewRectangle(color.RGBA{R: 255, G: 255, B: 255, A: 255})
-	secondaryRoadLine.SetMinSize(fyne.NewSize(height/100, height/3))
-
-	mainRoad := canvas.NewRectangle(color.RGBA{R: 18, G: 10, B: 5, A: 255})
-	mainRoad.SetMinSize(fyne.NewSize(width+10, height/5))
-
-	mainRoadLine := canvas.NewRectangle(color.RGBA{R: 255, G: 255, B: 255, A: 255})
-	mainRoadLine.SetMinSize(fyne.NewSize(width+10, height/100))
-
-	semaphoreBackground := canvas.NewRectangle(color.RGBA{R: 100, G: 100, B: 100, A: 255})
-	semaphoreBackground.SetMinSize(fyne.NewSize(70, 20))
-
-	redSemaphoreLight := canvas.NewRectangle(color.RGBA{R: 255, A: 255})
-	redSemaphoreLight.SetMinSize(fyne.NewSize(20, 20))
-
-	yellowSemaphoreLight := canvas.NewRectangle(color.RGBA{R: 255, G: 255, A: 60})
-	yellowSemaphoreLight.SetMinSize(fyne.NewSize(20, 20))
-
-	greenSemaphoreLight := canvas.NewRectangle(color.RGBA{G: 255, A: 60})
-	greenSemaphoreLight.SetMinSize(fyne.NewSize(20, 20))
-
-	topRightDownSemaphore := container.NewCenter(
-		semaphoreBackground,
-		container.NewHBox(
-			redSemaphoreLight,
-			yellowSemaphoreLight,
-			greenSemaphoreLight,
-		),
-	)
-
-	topRightLeftSemaphore := container.NewCenter(
-		semaphoreBackground,
-		container.NewHBox(
-			redSemaphoreLight,
-			yellowSemaphoreLight,
-			greenSemaphoreLight,
-		),
-	)
+	topRightDownSemaphore := Semaphore{}
+	topRightDownSemaphore.init()
+	topLeftRightSemaphore := Semaphore{}
+	topLeftRightSemaphore.init()
+	bottomRightLeftSemaphore := Semaphore{}
+	bottomRightLeftSemaphore.init()
+	bottomLeftTopSemaphore := Semaphore{}
+	bottomLeftTopSemaphore.init()
 
 	// El contenido de la aplicación
 	content := container.NewCenter(
 		container.NewVBox(
 			container.NewHBox(
-				leftGrass,
-				container.NewCenter(secondaryRoad, secondaryRoadLine),
-				rightGrass,
+				createGrassObject(),
+				createRoadObject(false),
+				createGrassObject(),
 			),
 			container.NewHBox(
-				container.NewCenter(mainRoad, mainRoadLine),
+				createRoadObject(true),
 			),
 			container.NewHBox(
-				leftGrass,
-				container.NewCenter(secondaryRoad, secondaryRoadLine),
-				rightGrass,
+				createGrassObject(),
+				createRoadObject(false),
+				createGrassObject(),
 			),
 		),
-		topRightDownSemaphore,
+		container.NewWithoutLayout(
+			createSemaphoreObject(&topRightDownSemaphore, false, false),
+			createSemaphoreObject(&topLeftRightSemaphore, true, true),
+			createSemaphoreObject(&bottomRightLeftSemaphore, true, false),
+			createSemaphoreObject(&bottomLeftTopSemaphore, false, true),
+		),
 	)
 
 	myWindow.SetContent(content)
